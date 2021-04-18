66a6b91852af99d5166eda6a19460ea5ed598ddf
Adrian Gomes <adrianhgomes@gmail.com>
2021-04-17 21:08:49 -0400

    Added options and error checks

--- c/secret-santa.c
+++ c/secret-santa.c
@@ -1,17 +1,101 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ncurses.h>
 #include <errno.h>
+#include <getopt.h>
+
+#define RED "\x1b[1;91m"
+#define GREEN "\x1b[3;92m"
+#define YELLLOW "\x1b[1;93m"
+#define DEFAULT "\x1b[0;0m"
+
+bool is_pos_integer(char *input) {
+    if (input[0] == '-') return false;
+
+    int len = strlen(input);
+    for (int i = 0; i < len; i++) {
+        if (!isdigit(input[i])) {
+            return false;
+        }
+    }
+    return true;
+}
 
 
-int main(int argc, char const *argv[]) {
-    FILE *roster;
-    if ((roster = fopen("roster.txt", "r")) == NULL) {
-        fprintf(stderr, "Error: Could not open '%s'. %s.\n", "roster.txt", strerror(errno));
+void display_usage(char *progname) {
+    printf("usage: %s [-h] [-f <path>] [-s <seed>] [-l [path]]\n", progname);
+    printf("  -f <path>  File to use for list of participants [default: 'roster.txt'].\n");
+    printf("  -l [path]  Creates file with list of all pairings [default: 'list.txt'].\n");
+    printf("  -s <seed>  Seed to use. Must be a positive integer.\n");
+    printf("  -h         Display help.\n");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc > 8) {
+        display_usage(argv[0]);
         return EXIT_FAILURE;
     }
-    
+
+    int opt = 0, lflag = 0;
+    unsigned int seed = 1;
+    char *fpath = NULL, *lpath = NULL, *s_val = NULL;
+
+    while ((opt = getopt(argc, argv, ":l:f:s:h")) != -1) {
+        switch (opt) {
+            case 'h':
+                display_usage(argv[0]);
+                return EXIT_SUCCESS;
+            case 'l':
+                lflag = 1;
+                lpath = optarg;
+                break;
+            case 'f':
+                fpath = optarg;
+                break;
+            case 's':
+                s_val = optarg;
+                break;
+            case '?':
+                fprintf(stderr, "%sError:%s Unknown option '-%c' received.\n", RED, DEFAULT, optopt);
+                return EXIT_FAILURE;
+            default:
+                if (optopt == 'l') break;
+                fprintf(stderr, "%sError:%s Option '-%c' requires an argument.\n", RED, DEFAULT, optopt);
+                return EXIT_FAILURE;
+        }
+    }
+
+    if (lpath != NULL && lpath[0] == '-') {
+        fprintf(stderr, "%sWarning:%s '%s' is an irregular path for list file. [usage ex: 'list.txt'].\n", YELLLOW, DEFAULT, lpath);
+    }
+
+    if (s_val != NULL) {
+        if (!is_pos_integer(s_val)) {
+            fprintf(stderr, "%sError:%s Seed must be a postive integer.\n", RED, DEFAULT);
+            return false;
+        }
+        seed = (unsigned int)atoi(s_val);
+    }
+
+    if (optind < argc) {
+        fprintf(stderr, "%sError:%s Too many arguments have been supplied or incorrect format.\n       Use '-h' to check usage.\n", RED, DEFAULT);
+        return EXIT_FAILURE;
+    }
+
+    if (lflag) {
+        printf("%sList functionality is not currently implemented.%s\n", GREEN, DEFAULT);
+        return EXIT_SUCCESS;
+    }
+
+    FILE *roster;
+    if (fpath == NULL) fpath = "roster.txt";
+    if ((roster = fopen(fpath, "r")) == NULL) {
+        fprintf(stderr, "%sError:%s Could not open '%s'. %s.\n", fpath, RED, DEFAULT, strerror(errno));
+        return EXIT_FAILURE;
+    }
+
     int lines = 0;
     char buf[256];
     while(fgets(buf, sizeof(buf), roster) != NULL) {
@@ -19,24 +103,25 @@ int main(int argc, char const *argv[]) {
     }
     buf[strcspn(buf, "\n")] = 0;
     if (lines < 2) {
-        printf("Not enough participants to play :( \n");
+        fprintf(stderr, "%sError:%s Not enough participants to play.\n", RED, DEFAULT);
         return EXIT_FAILURE;
     }
 
     fclose(roster);
-    roster = fopen("roster.txt", "r");
+
+    if ((roster = fopen(fpath, "r")) == NULL) {
+        fprintf(stderr, "%sError:%s Could not open '%s'. %s.\n", fpath, RED, DEFAULT, strerror(errno));
+        return EXIT_FAILURE;
+    }
     char names[lines][50];
     for(int i = 0; i < lines && fgets(names[i], 50, roster) != NULL; i++) {
         names[i][strlen(names[i]) - 1] = '\0';
     }
     fclose(roster);
-    for (int i = 0; i < lines; i++) {
-        printf("%s\n", names[i]);
-    }
 
     int idxs[lines];
     for(int i = 0; i < lines; i++) idxs[i] = i;
-    srandom(95);
+    srandom(seed);
     long r = random();
     char snames[lines][50];
     for(int i = lines - 1; i >= 0; i--) {
@@ -72,5 +157,8 @@ int main(int argc, char const *argv[]) {
         clear();
     } while(c != 'q');
     endwin();
+
+    printf("%ssecret-santa closed.%s\n", GREEN, DEFAULT);
+
     return EXIT_SUCCESS;
 }
